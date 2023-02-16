6f40e17f89cb75da84a4f48d002b56f7df156610
nousr <51308183+nousr@users.noreply.github.com>
2023-02-16 01:43:35 +0000

    add example usage script

---
+++ example.py
@@ -0,0 +1,12 @@
+from oh_clap import CLAPLoader
+
+DATASET_URL = "s3://s-laion-audio/webdataset_tar/LJSpeech/train/{0..5}.tar"
+
+def main():
+    "Try out the CLAPLoader."
+
+    # Create a CLAPLoader
+
+
+if __name__ == "__main__":
+    main()
